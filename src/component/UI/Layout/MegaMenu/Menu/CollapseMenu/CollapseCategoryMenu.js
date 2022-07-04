a78a24de34e73f135da62d1600d62571fea55601
troy <hunter007nz@gmail.com>
2022-07-04 15:26:57 +1200

    Shopify-React-Site

--- src/component/UI/Layout/MegaMenu/Menu/CollapseMenu/CollapseCategoryMenu.js
+++ src/component/UI/Layout/MegaMenu/Menu/CollapseMenu/CollapseCategoryMenu.js
@@ -1,97 +1,73 @@
-import React from 'react'
+import React from "react"
 
-import { Div, Tag, Anchor, Icon, Button, Dropdown, Row, Col } from "atomize";
-import { Link } from "react-router-dom";
+import { Div, Tag, Anchor, Icon, Button, Dropdown, Row, Col } from "atomize"
+
+import { Link } from "react-router-dom"
 
 const CollapseCategoryMenu = (props) => {
-    const { menuProps } = props;
+  const { menuProps } = props
 
+  const hoverdHandler = (id, index) => {
+    props.hoverdHandler(id, "SubMenu", index)
+  }
 
-    const hoverdHandler = (id, index) => {
+  //   const SubCategory = (props) => {
+  //     return (
+  //       <div>
+  //         {props.props.list.map(
+  //           (x) =>
+  //             x.subMenuIsHovered === true &&
+  //             x.subCategory.map((x) => <li>{x.title} </li>)
+  //         )}
+  //       </div>
+  //     )
+  //   }
 
-        props.hoverdHandler(id, "SubMenu", index,);
-    };
+  const SubMenuList = (props) => {
+    let subMenuItems = []
 
-    const SubCategory = (props) => {
-
-
-        console.log("x", props )
-        console.log("subCategory", props.props.list )
-        return (
-            <div>
-                {props.props.list.map(x => x.subMenuIsHovered === true && x.subCategory.map(x => <li>{x.title} </li>))}
-           
-            </div>
-        )
+    if (props.props !== undefined) {
+      const { list } = props.props
+      const { subMenuIsHovered } = props.props
+      subMenuItems = list
     }
 
-    const SubMenuList = (props) => {
-        let subMenuItems = []
+    return (
+      <>
+        {subMenuItems !== undefined &&
+          subMenuItems.map((x, i) => {
+            return (
+              <>
+                <Link key={x.id} to={x.url}>
+                  <Anchor
+                    textColor="black"
+                    d="block"
+                    p={{ y: "0.25rem" }}
+                    onMouseEnter={() => hoverdHandler(x.id, i)}
+                  >
+                    {x.title}
+                  </Anchor>
+                </Link>
+              </>
+            )
+          })}
+      </>
+    )
+  }
 
+  const MenuList = (props) => {
+    const [MainSublist] = props.props
 
-        if (props.props !== undefined) {
-            const { list } = props.props;
-            const { subMenuIsHovered } = props.props;
-            subMenuItems = list
-
-
-        }
-
-
-        return (
-            <>
-                {subMenuItems !== undefined &&
-                    subMenuItems.map((x, i) => {
-
-                        return (
-
-                            <>
-
-
-                                <Link key={x.id} to={x.url}>
-                                    <Anchor
-                                        textColor="black"
-                                        d="block"
-                                        p={{ y: "0.25rem" }}
-                                        onMouseEnter={() => hoverdHandler(x.id, i)}
-                                    >
-                                        {x.title}
-                                    </Anchor>
-                                </Link>
-
-
-
-
-                            </>
-
-                        )
-                    })}
-
-
-            </>
-        )
-    }
-
-    const MenuList = (props) => {
-        const [MainSublist] = props.props;
-
-        return (
-            <Row>
-
-
-
-
-                <Col>
-
-                    <SubMenuList props={MainSublist} />
-                </Col>
-                <Col>
-                    <SubCategory props={MainSublist} />
-                </Col>
-            </Row>
-        );
-    }
-    return <MenuList props={menuProps} />
-};
+    return (
+      <Row>
+        <Col>
+          <SubMenuList props={MainSublist} />
+        </Col>
+        <Col>{/* <SubCategory props={MainSublist} /> */}</Col>
+      </Row>
+    )
+  }
+  return <MenuList props={menuProps} />
+}
 
 export default CollapseCategoryMenu
